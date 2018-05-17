be54c3042a9d3bb6d8d4a76f2711b41d79b9e360
dfathu <dfathu@gmail.com>
2018-05-17 15:45:25 +0700

    niomic.id tutor : 1. menjalankan console log dengan variabel 2. function file (test.js & function.js)

---
+++ function.js
@@ -0,0 +1,18 @@
+function Jalansatu(){
+  console.log("jalan satu")
+}
+
+const jalanDua = function (){
+  console.log("jalanDua")
+}
+
+Jalansatu()
+jalanDua()
+
+
+
+const jalanTiga =  ()=>{
+
+  console.log("jalan tiga")
+}
+jalanTiga()
