d2c8c7b0c8bc4d32d37b5044d70ba0b6cfbce9dd
dfathu <dfathu@gmail.com>
2018-05-17 16:33:07 +0700

    niomic.id tutor : 5. For 6. Array file (for.js & array.js)

---
+++ array.js
@@ -0,0 +1,14 @@
+function Aray(){
+  var data = [1,2,3,4,5,0]
+  var nama = ["abi","abi2","abi3"]
+  console.log(data.length)
+  console.log(nama.length)
+  for (var i = 0; i < data.length; i++) {
+    console.log(data[i])
+  }
+  for (var i = 0; i < nama.length; i++) {
+    console.log(nama[i]) 
+  }
+
+}
+Aray()
---
+++ for.js
@@ -0,0 +1,9 @@
+function perulangan(){
+  for (var i = 10; i > 0; i--) {
+    console.log("Jalan : ",i)
+  }
+  for (var i = 0; i <= 10; i++) {
+    console.log("Jalan : ",i)
+  }
+}
+perulangan()
